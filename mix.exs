bb665f61833cd98b12049fdce65f1f86d1b4f06f
Jaap Frolich <jaap@she-corp.com>
2018-03-09 00:10:03 +0800

    updating the package metadata

--- mix.exs
+++ mix.exs
@@ -4,17 +4,23 @@ defmodule Deferred.MixProject do
   def project do
     [
       app: :defer,
-      description: "Deferrables for Elixir",
-      files: ["lib", "priv", "mix.exs", "README.md", "CHANGELOG.md", ".formatter.exs"],
-      maintainers: [
-        "Jaap Frolich"
-      ],
-      licenses: ["MIT"],
-      links: %{github: "https://github.com/jfrolich/defer"},
+      package: package(),
       version: "0.1.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps()
+    ]
+  end
+
+  defp package() do
+    [
+      description: "Deferrables for Elixir",
+      files: ["lib", "mix.exs", "README.md", ".formatter.exs"],
+      maintainers: [
+        "Jaap Frolich"
+      ],
+      licenses: ["MIT"],
+      links: %{github: "https://github.com/jfrolich/defer"}
     ]
   end
 
@@ -25,6 +31,8 @@ defmodule Deferred.MixProject do
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
-    []
+    [
+      {:ex_doc, ">= 0.0.0", only: :dev}
+    ]
   end
 end
